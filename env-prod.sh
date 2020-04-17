cb555f519eb56e34e7e1f634024735fdb0596b09
dang ngoc tuan <ngoc-tuan.dang@ant-tech.eu>
2020-04-18 00:16:37 +0700

    adsadsa

---
+++ env-prod.sh
@@ -0,0 +1,13 @@
+export PORT=3001
+export MONGO_URI=mongodb+srv://learn-japanese-admin:DV52E6RREE@cluster0-mpdqn.mongodb.net/learn-japanese-dev
+export TOKEN_SECRET=TlhvN_k16
+export TOKEN_EXPIRED=7d
+export TOKEN_ADMIN_SECRET=aDmIn_TlhvN_k16
+export TOKEN_ADMIN_EXPIRED=30d
+export MAJOR_MINOR_PATCH=1.0.0
+export ENV=DEV
+export SERVER_URL=http://192.168.1.234
+export REDIS_HOST=redis
+export REDIS_PORT=6379
+export MONGO_PORT=27017
+export NGINX_PORT=80
